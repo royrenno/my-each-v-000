9e3388e2b0ecead7a7db0df11d64e0670d9da1fb
roy renno <royrenno@yahoo.com>
2019-02-07 01:14:12 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,11 @@
 def my_each (array)
-  array = [2,3,4,5]
-  my_each(array) do |i|
-    puts i
+  i = 0
+  while i < array.length
+    yeild (array [i])
+    i = i+1
   end
-end
+  array
+  end
 
   # put argument(s) here
   # code her
